0c9bae782623b4864375fb85923e21a62defc943
bufi <69212404+Beby238@users.noreply.github.com>
2025-09-07 16:18:18 +0000

    Add files via upload

---
+++ ESP32-CAM/ESP32_CAM_NOW/espnow_photo_sender.ino
@@ -0,0 +1,246 @@
+#include <esp_now.h>
+#include "esp_camera.h"
+#include <WiFi.h>
+
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
+
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+#define maxPackageSize 240.0
+
+// Esp32S3, kann auch automatisiert verlaufen mit SSID
+uint8_t broadcastAddress[] = {0xFC, 0x01, 0x2C, 0xD1, 0xF6, 0xD4};
+
+camera_fb_t *fb = NULL;
+
+//Daten von Peer speichern (für die Verbindung wichtig)
+esp_now_peer_info_t peerInfo;
+
+uint8_t tookPhotoFlag = 0; //0 = Kein Foto aufnehmen
+uint8_t issending = 0;
+int currentTransmitPosition = 0;
+int totalTransmitPackages = 0;
+uint8_t sendnextPackageFlag = 0;
+
+void initCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  Serial.println("psramFound() = " + String(psramFound()));
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_QVGA; //FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA //FRAMESIZE_QVGA
+    config.jpeg_quality = 2;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+    // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+
+void takePhoto(){
+  tookPhotoFlag = 0;
+  fb = esp_camera_fb_get();
+  if (!fb){
+    Serial.println("Camera capture failed");
+  }
+}
+
+// Wird später implementiert
+void cuttingPhoto(){
+
+}
+
+void transmitting(){
+  int photosize = fb->len;
+  Serial.println("Größe des Fotos: " + photosize);
+  currentTransmitPosition = 0;
+  totalTransmitPackages = ceil(photosize / maxPackageSize);
+  Serial.println("Packetanzahl: " +  totalTransmitPackages);
+  uint8_t message[] = {0x01, totalTransmitPackages >> 8, (byte) totalTransmitPackages}; // Wird auf 3 Bytes geteilt,
+  // beim Empfänger wird totalTransmitPackages wieder aufgebaut.
+
+
+  //Empfänger sagen, dass eine Datei gesendet wird
+  sendData(message, sizeof(message));
+
+}
+
+// Reaktion vom Empfänger
+void OnDataSent(const uint8_t * mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  
+  if (totalTransmitPackages){ //Falls es eine Datei gibt zum versenden
+    tookPhotoFlag = 1;
+    
+    // wenn Error, sende letztes Packet nochmal
+    if (status != ESP_NOW_SEND_SUCCESS){
+      currentTransmitPosition--;
+    }
+  }
+}
+
+void sendNextPackage(){
+  sendnextPackageFlag = 0;
+
+  //if is after the last package
+  if (currentTransmitPosition == totalTransmitPackages){
+    currentTransmitPosition = 0;
+    totalTransmitPackages = 0;
+    Serial.println("Datei gesendet.");
+    esp_camera_fb_return(fb);
+    return;
+  }
+
+  //Array Packet senden:
+  int dataSize = maxPackageSize;
+  //Wenn es die letzte Datei ist, muss das Paket angepasst werden
+  if (currentTransmitPosition == totalTransmitPackages - 1){
+    Serial.println("************************");
+    Serial.println(totalTransmitPackages - 1);
+    dataSize = fb->len - ((totalTransmitPackages - 1) - maxPackageSize);
+
+    Serial.println(" last dataSize Package: " + dataSize);
+
+    //datamessagearray
+    uint8_t messageArray[dataSize + 3]; // die ersten 3 Einträge ist die Übermittlung von Operator und derzeitige Position vom Paket (16 Bit aufgeteilt auf 2 Byte)
+    messageArray[0] = 0x02;
+
+    //hier wäre file.seek und currentTransmitPosition die um eins erhöht wird
+    currentTransmitPosition ++;
+
+    messageArray[1] = currentTransmitPosition >> 8;
+    messageArray[2] = (byte) currentTransmitPosition;
+
+    uint16_t pos = currentTransmitPosition * maxPackageSize; //AChtung falls die Bilder größer oder mehrere gesendet wird
+
+    for (int i = 0; i < dataSize; i++){
+        messageArray[3+i] = fb->buf[pos + i];
+    }
+
+    sendData(messageArray, sizeof(messageArray));
+  }
+}
+
+//Nur für das senden zuständig
+void sendData(uint8_t* dataArray, uint8_t arrayLength){
+  //const uint8_t *peer_addr = slave.peer_addr; //Benötigt?
+
+  esp_err_t result = esp_now_send(broadcastAddress, dataArray, arrayLength);
+
+  if (result == ESP_OK) {
+    //Serial.println("Success");
+  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+    // How did we get so far!!
+    Serial.println("ESPNOW not Init.");
+  } else if (result == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+    Serial.println("Internal Error");
+  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+    Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer not found.");
+  } else {
+    Serial.println("Not sure what happened");
+  }
+  //möglichweise wird delay verwendet für ESPNOW
+  //delay(1000);
+}
+
+
+void setup(){
+  Serial.begin(115200);
+
+  initCamera();
+
+
+  //ESP zu Wi-Fi Station setzen, wichtig für NOW 
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  //neuere Methode für callback hinzufügen
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent)); // esp_now_register_send_cb(OnDataSent);
+
+    // Register peer, kann durch automatische Suche ersetzt werden
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+    // Add peer, kann durch automatische Suche ersetzt werden        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void loop(){
+
+
+
+  //foto Schießen, falls weder gesendet oder noch kein Foto geschossen wurde
+  if (!sendnextPackageFlag && !tookPhotoFlag && Serial.available() > 0 && Serial.read() == 'f'){
+    tookPhotoFlag = 1;
+    takePhoto();
+  }
+
+  //Falls nächstes Packet gesendet wird
+  if(sendnextPackageFlag){
+    sendNextPackage();
+  }
+}
+
